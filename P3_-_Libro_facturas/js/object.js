726098e32c03b445c948dc27bd6e51532f13adba
hesselek <hesselek@gmail.com>
2014-01-28 23:48:19 +0100

    pocos cambios...

--- P3_-_Libro_facturas/js/object.js
+++ P3_-_Libro_facturas/js/object.js
@@ -3,13 +3,44 @@
  */
 
 
-function cliente(sDireccion, sNif, sNombre){
+function Cliente(sDireccion, sNif, sNombre){
 	this.direccion = sDireccion;
 	this.nif = sNif;
 	this.nombre = sNombre;
 }
 
-function producto(sPrecio,sDescripcion){
-	this.precio = sPrecio;
+function Producto(dbPrecio,sDescripcion){
+	this.precio = dbPrecio;
 	this.descripcion =sDescripcion;
 }
+
+function Servicio(dbPrecio,sDescripcion){
+	this.precio = dbPrecio;
+	this.descripcion =sDescripcion;
+}
+
+function Linea(oConcepto){
+	this.total = oConcepto.precio;
+	this.descripcion = oConcepto.descripcion;
+}
+
+function LineaProducto (oConcepto,iUnidades) {
+	Call(this,oConcepto);
+	this.cantidad = iUnidades;
+}
+
+function LineaServicio(oConcepto,iUnidades) {
+	Call(this,oConcepto);
+}
+
+function Factura (oCliente,iCodigo) {
+	this.cliente = oCliente;
+	this.codigo = iCodigo;
+  
+}
+function Libro(){
+	
+}
+var miProducto = new producto(12.3,"Esta es mi descripcion");
+alert(miProducto.descripcion);
+var miLineaProducto = new lineaProducto(miProducto);
