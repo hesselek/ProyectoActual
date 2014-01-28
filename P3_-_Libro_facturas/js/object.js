a605c254be5ec35527852b2ff7fad650f8aa3565
hesselek <hesselek@gmail.com>
2014-01-29 00:13:27 +0100

    otra cosa mas

--- P3_-_Libro_facturas/js/object.js
+++ P3_-_Libro_facturas/js/object.js
@@ -38,9 +38,95 @@ function Factura (oCliente,iCodigo) {
 	this.codigo = iCodigo;
   
 }
-function Libro(){
-	
+
+
+// y ahora, una pequeña implementacion de una clase tipo Linked List.., que me permite una mejor implementacion
+// y un objeto reutilizable.
+
+function List() {
+ List.nodo = function() { 
+  return {data: null, next: null}; 
+ }; 
+ 
+ this.start = null; 
+ this.end = null; 
 }
+
+ List.prototype.add = function(data) { 
+  if (this.start === null) { 
+   this.start = List.nodo(); 
+   this.end = this.start; 
+  } else { 
+   this.end.next = List.makeNode(); 
+   this.end = this.end.next; 
+  } ; 
+  this.end.data = data; 
+ };
+
+ List.prototype.borrar = function(data) { 
+  var current = this.start; 
+  var previous = this.start; 
+  while (current !== null) { 
+   if (data === current.data) { 
+    if (current === this.start) { 
+     this.start = current.next; 
+     return; 
+    } 
+    if (current === this.end) 
+                      this.end = previous;
+    previous.next = current.next; return; 
+    }
+    previous = current; 
+    current = current.next; 
+   }
+ }; 
+
+ List.prototype.insertAsFirst = function(d) { 
+  var temp = List.makeNode(); 
+  temp.next = this.start; 
+  this.start = temp; 
+  temp.data = d; 
+ };
+ 
+  function Libro(){
+	 this.listaFacturas = new List();
+	 this.posicion = -1;
+  }
+	
+/*
+ this.insertAfter = function(t, d) { 
+  var current = this.start; 
+  while (current !== null) { 
+   if (current.data === t) { 
+    var temp = List.makeNode();
+    temp.data = d; 
+    temp.next = current.next; 
+    if (current === this.end) this.end = temp;
+    current.next = temp; 
+    return; 
+   } 
+   current = current.next; 
+   }
+  };
+
+  this.item = function(i) { 
+   var current = this.start; 
+   while (current !== null) { 
+    i--; 
+    if (i === 0) return current; 
+    current = current.next; 
+   } 
+   return null; 
+  }; 
+
+ this.each = function(f) {
+  var current = this.start;
+  while (current !== null) { 
+   f(current); 
+   current = current.next; 
+  } 
+ };
+} */
 var miProducto = new producto(12.3,"Esta es mi descripcion");
 alert(miProducto.descripcion);
 var miLineaProducto = new lineaProducto(miProducto);
