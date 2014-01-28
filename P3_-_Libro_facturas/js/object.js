2b8aa8679cc24ec35905b89440499972ff1b4417
hesselek <hesselek@gmail.com>
2014-01-28 18:07:47 +0100

    a ver si acaso

---
+++ P3_-_Libro_facturas/js/object.js
@@ -0,0 +1,15 @@
+/**
+ * @author hesselek
+ */
+
+
+function cliente(sDireccion, sNif, sNombre){
+	this.direccion = sDireccion;
+	this.nif = sNif;
+	this.nombre = sNombre;
+}
+
+function producto(sPrecio,sDescripcion){
+	this.precio = sPrecio;
+	this.descripcion =sDescripcion;
+}
