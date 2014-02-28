610a14b83fcbbb1bb5a2c57659801c1b719d60b5
hesselek <hesselek@gmail.com>
2014-02-28 16:12:23 +0100

    Terminado, mas o menos, la parte del servidor

--- web_tiempo/eltiempo.php
+++ web_tiempo/eltiempo.php
@@ -1,26 +1,69 @@
 <?php
 
-$json_entrada = $_POST['datos'];  // $json_entrada --> cadena en JSON
+$json_entrada = $_POST['datos'];  
 
-$objeto = json_decode($json_entrada);  // convertimos a objeto
+$objeto = json_decode($json_entrada);  
 
 $resultado = $objeto->localidad;
 
 $tiempo =  simplexml_load_file("http://www.aemet.es/xml/municipios/localidad_".$resultado.".xml");
 
-$today = array();
-$tomorrow = array();
-if(date("H")<=12)
-	$today['predicion'] = $tiempo->prediccion->dia[0]->estado_cielo[1]['descripcion'];
-else
-	$today['predicion'] = $tiempo->prediccion->dia[1]->estado_cielo[2]['descripcion'];
+/*
+ * Voy a parsear desde el servidor el contenido del xml, para poder sacar un objeto JSON.
+ */
+ 
+ /*
+  * Esto me permite accedar al dato concreto, según la hora del día en que se trate
+  */
+$hora = date("H");
+$periodo = 3;
+if($hora>6)
+	$periodo++;
+if($hora>12)
+	$periodo++;
+if($hora>18)
+	$periodo++;
 
-$tomorrow['predicion'] = $today['predicion'] = $tiempo->prediccion->dia[0]->estado_cielo[0]['descripcion'];
 
-$predicion = $tiempo->prediccion->dia[0]->estado_cielo[2]['descripcion'];
-$predicion1 = $tiempo->prediccion->dia[1]->estado_cielo[2]['descripcion'];
+	
 
-$objeto_salida = array($today,$tomorow);
-echo json_encode($objeto_salida);// $contenido;
+	$objeto_salida = array();
+	/*
+	 * En este array se almacenan tres valores de los que necesitamos tres datos
+	 */
+	 
+	 $array_tem = array("temperatura","sens_termica","humedad_relativa");
+	 
+	 /*
+	 * Es necesario castear el contenido de ciertas variables, pues para php son objetos, y al codificarlo,
+	  * se hacía un puñetero lio
+	 */
+	
+	for($i=0;$i<2;$i++){
+		$predic = $tiempo->prediccion->dia[$i];
+		$predic_dia['prob_precipitacion'] = (String)$predic->prob_precipitacion[$periodo];
+		$predic_dia['descripcion'] = (String) $predic->estado_cielo[$periodo]['descripcion'];
+		$predic_dia['direccion_v'] = (String) $predic->viento[$periodo]->direccion;
+		$predic_dia['velocidad_v'] = (string) $predic->viento[$periodo]->velocidad;
+		
+		foreach ($array_tem as $key => $value) {
+			$arr_aux = array();
+			$arr_aux['maxima'] = (String) $predic->$value->maxima;
+			$arr_aux['minima'] = (String) $predic->$value->minima;
+			$arr_aux['actual'] = (String) $predic->$value->dato[$periodo-3];
+			
+			$predic_dia[$value] = $arr_aux;
+			
+		}
+
+		array_push($objeto_salida,$predic_dia);
+	}
+
+
+
+
+
+
+echo json_encode($objeto_salida);
 
 ?>
--- web_tiempo/mitiempo.php
+++ web_tiempo/mitiempo.php
@@ -6,19 +6,59 @@ $tiempo =  simplexml_load_file("localidad_28013.xml");
 
 
 /*
- * Voy a parsear desde el servidor el contenido del xml
+ * Voy a parsear desde el servidor el contenido del xml, para poder sacar un objeto JSON.
  */
+ 
+ /*
+  * Esto me permite cre
+  */
+$hora = date("H");
+$periodo = 3;
+if($hora>6)
+	$periodo++;
+if($hora>12)
+	$periodo++;
+if($hora>18)
+	$periodo++;
 
-//if(date("H")<=12)
-	//$today['predicion'] = $tiempo->prediccion->dia[0]->estado_cielo[1]['descripcion'];
-//else
-	$today[0] = $tiempo->prediccion->dia[0]->estado_cielo[2]['descripcion'];
 
-$tomorrow[0] =  $tiempo->prediccion->dia[0]->estado_cielo[2]['descripcion'];
+	/*
+	 * Es necesario castear el contenido de ciertas variables, pues para php son objetos, 
+	 */
+
+	$objeto_salida = array();
+	/*
+	 * En este array se almacenan tres valores de los que necesitamos tres datos
+	 */
+	 
+	 $array_tem = array("temperatura","sens_termica","humedad_relativa");
+	
+	for($i=0;$i<2;$i++){
+		$predic = $tiempo->prediccion->dia[$i];
+		$predic_dia['prob_precipitacion'] = (String)$predic->prob_precipitacion[$periodo];
+		$predic_dia['descripcion'] = (String) $predic->estado_cielo[$periodo]['descripcion'];
+		$predic_dia['direccion_v'] = (String) $predic->viento[$periodo]->direccion;
+		$predic_dia['velocidad_v'] = (string) $predic->viento[$periodo]->velocidad;
+		
+		foreach ($array_tem as $key => $value) {
+			$arr_aux = array();
+			$arr_aux['maxima'] = (String) $predic->$value->maxima;
+			$arr_aux['minima'] = (String) $predic->$value->minima;
+			$arr_aux['actual'] = (String) $predic->$value->dato[$periodo-3];
+			
+			$predic_dia[$value] = $arr_aux;
+			
+		}
+
+		array_push($objeto_salida,$predic_dia);
+	}
 
 
-$objeto_salida = array($today,$tomorrow);
+
+
+
+
 echo json_encode($objeto_salida);
-echo date("H:m:s");
+
 
 ?>
